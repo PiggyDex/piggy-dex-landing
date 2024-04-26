7ebf5bfd28af43d96a9942d218e319b966da50a5
KevinHuang <eevee.vt.pt@gmail.com>
2024-04-26 21:41:59 +0700

    fix: eslint react

--- src/components/role-and-contact/index.tsx
+++ src/components/role-and-contact/index.tsx
@@ -1,5 +1,15 @@
 import { type FC } from "react";
 
 export const RoleAndContact: FC = () => {
-  return <div>Role and Contact</div>;
+  return (
+    <div className="container flex flex-col items-center">
+      <div className="flex flex-col gap-16">
+        <span>
+          <span>Piggy DEX</span>
+          plays a vital role in powering the <br /> revolution of Defi on
+          Conflux
+        </span>
+      </div>
+    </div>
+  );
 };
