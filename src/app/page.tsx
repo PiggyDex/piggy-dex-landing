478ba1fe35011d72791a8ef4537e446b4285fdf3
KevinHuang <eevee.vt.pt@gmail.com>
2024-04-26 21:41:59 +0700

    fix: eslint react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-between overflow-x-hidden bg-gradient-to-t from-[#FFA6C6] via-[#261E21] via-90%">
         <Earn />
         <Spacer className="h-[300px]" />
+        {/* <Spacer className="h-[300px]" /> */}
         <RoleAndContact />
         <Spacer />
         <Footer />
